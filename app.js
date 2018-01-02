92d73a5407855ec39be338831cf5388f206274b2
FREEWAVE\iaguerrevere <iaguerrevere@freewave.com>
2018-01-02 10:58:42 -0700

    added file structure for LE challenge

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/health-check", (req, res) => res.sendStatus(200));
+
+app.use(express.static("static"));
+
+app.listen(8080);
