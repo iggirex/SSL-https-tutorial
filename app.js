131c26730e58d54904311e2dd0ad05bcf20b939d
Ig <iggirex@gmail.com>
2018-01-03 10:43:33 -0700

    fixed acme challenge route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/health-check", (req, res) => res.sendStatus(200));
+//app.get("/health-check", (req, res) => res.sendStatus(200));
 
 app.use(express.static("static"));
 
